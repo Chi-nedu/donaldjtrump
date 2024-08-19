b196d257ced5208884cedb2f93f3d39fd62d5d8d
Okwor Chinedu <devplus001@gmail.com>
2024-08-19 02:31:40 +0100

    finished

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,26 @@
 import Homepage from '@/components/homepage'
 
+// export const metadata = {
+//   title: "Donald J. Trump",
+//   description: "Making sure Americans are living the American dream. ",
+// };
+
 export const metadata = {
   title: "Donald J. Trump",
-  description: "Making sure Americans are living the American dream. ",
+  description: "Making sure Americans are living the American dream.",
+  openGraph: {
+    title: "Donald J. Trump",
+    description: "Making sure Americans are living the American dream.",
+    type: "website",
+    images: [
+      {
+        url: "/images/og.png",
+        width: 800,
+        height: 600,
+        alt: "Donald J. Trump",
+      },
+    ],
+  },
 };
 
 
